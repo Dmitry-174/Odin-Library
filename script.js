17692e04d6ec5152d66d9150d1702f1dfd406ad8
Dmitrii <dmitry.valiev174@gmail.com>
2022-11-05 21:07:25 +0500

    Make adding new book

--- script.js
+++ script.js
@@ -52,14 +52,47 @@ function createBookCard(book) {
 }
 
 function showBooks(library) {
+    let cards = document.querySelector(".cards");
+    while (cards.firstChild) {
+        cards.removeChild(cards.lastChild)
+    }
+
     for (let book of library) {
         let card = createBookCard(book);
-        let cards = document.querySelector(".cards");
         cards.appendChild(card);
     }
 }
 
-
 addBookToLibrary('The Hobbit', "J.R.R. Tolkien", 295, false);
 addBookToLibrary('The Test', "T. Test", 25, true);
 showBooks(myLibrary);
+
+let addBookForm = document.querySelector('.add-book-form');
+let overlay = document.querySelector('.overlay');
+let addBtn = document.querySelector('.add-btn');
+
+addBtn.addEventListener("click", (e) => {
+    addBookForm.classList.add("active");
+    overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", (e) => {
+    if (addBookForm.classList.contains("active")) {
+        addBookForm.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+})
+
+let addSubmitBtn = document.querySelector("#add-submit-btn");
+addSubmitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let title = addBookForm[0].value;
+    let author = addBookForm[1].value;
+    let pages = addBookForm[2].value;
+    let read = addBookForm[3].checked;
+    myLibrary.push(new Book(title, author, pages, read));
+    addBookForm.classList.remove("active");
+    overlay.classList.remove("active");
+    showBooks(myLibrary);
+
+})
